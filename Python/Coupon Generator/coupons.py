cc8cfcc5c780d46122f95df67b7385bfeaf044ab
htootayzaaung <htootayzaaung.01@gmail.com>
2023-08-31 22:12:44 +0630

    Coupon generator finalised...

--- Python/Coupon Generator/coupons.py
+++ Python/Coupon Generator/coupons.py
@@ -9,12 +9,13 @@ def generate_coupon(c, x, y, width, height, company, coupon_id, date_str):
     c.rect(x, y, width, height)
 
     # Use default font
-    c.setFont("Helvetica-Bold", 14)
+    c.setFont("Helvetica-Bold", 16)
     
     # Draw texts on the coupon
     c.drawString(x + 10, y + height - 20, company)
-    c.setFont("Helvetica", 12)
+    c.setFont("Helvetica-Bold", 14)
     c.drawString(x + 10, y + (height // 2), f"{coupon_id}")
+    c.setFont("Helvetica", 12)
     c.drawString(x + width - 80, y + 10, date_str)
 
 def generate_pdf(start_id, end_id, filename):
@@ -24,7 +25,7 @@ def generate_pdf(start_id, end_id, filename):
 
     c = canvas.Canvas(filename, pagesize=landscape(letter))
     
-    x_offset, y_offset = 50, 400  # starting position
+    x_offset, y_offset = 50, 450  # starting position
 
     for i in range(start_id, end_id + 1):
         generate_coupon(c, x_offset, y_offset, width, height, company, i, date_str)
@@ -34,9 +35,9 @@ def generate_pdf(start_id, end_id, filename):
             x_offset = 50
             y_offset -= height
         
-        if (i - start_id + 1) % 16 == 0:  # New page after 16 coupons
+        if (i - start_id + 1) % 20 == 0:  # New page after 16 coupons
             c.showPage()
-            x_offset, y_offset = 50, 400
+            x_offset, y_offset = 50, 450
 
     c.save()
 
