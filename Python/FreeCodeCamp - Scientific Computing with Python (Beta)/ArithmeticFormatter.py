2990892a6dd7f10d694f65c46ffda3a50fdd6fe3
htootayzaaung <htootayzaaung.01@gmail.com>
2024-10-02 00:25:55 +0100

    Solution Obtained: running the tests!

--- Python/FreeCodeCamp - Scientific Computing with Python (Beta)/ArithmeticFormatter.py
+++ Python/FreeCodeCamp - Scientific Computing with Python (Beta)/ArithmeticFormatter.py
@@ -1,65 +1,78 @@
 def arithmetic_arranger(problems, show_answers=False):
+    first_numbers = []
+    second_numbers = []
+    operators = []
+    solutions = []
+    dash_gaps = []
+    vertical_gap = 4
+    final_string = ""
 
-    return problems
+    # Check if there are more than 5 problems
+    if len(problems) > 5:
+        return "Error: Too many problems."
 
-# print(f'\n{arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])}')
+    # Separate the numbers and operators
+    for problem in problems:
+        if '+' in problem or '-' in problem:
+            splitted_numbers = problem.split(' ')
+            first_num = splitted_numbers[0]
+            operator = splitted_numbers[1]
+            second_num = splitted_numbers[2]
+            
+            # Check if the numbers contain only digits
+            if not first_num.isdigit() or not second_num.isdigit():
+                return "Error: Numbers must only contain digits."
+            
+            # Check if the numbers are more than 4 digits
+            if len(first_num) > 4 or len(second_num) > 4:
+                return "Error: Numbers cannot be more than four digits."
+            
+            # Append the numbers and operators
+            first_numbers.append(first_num)
+            operators.append(operator)
+            second_numbers.append(second_num)
+        else:
+            return "Error: Operator must be '+' or '-'."
 
-problems = ["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]
+    # Calculate the solutions if show_answers is True
+    if show_answers:
+        for i in range(len(operators)):
+            if operators[i] == '+':
+                solutions.append(int(first_numbers[i]) + int(second_numbers[i]))
+            elif operators[i] == '-':
+                solutions.append(int(first_numbers[i]) - int(second_numbers[i]))
 
-first_numbers = []
-second_numbers = []
-operators = []
-solutions = []
-dash_gaps = []
-vertical_gap = 4
-final_string = ""
-    
-for problem in problems:
-    if problem.find('+') is not None:
-        splitted_numbers = problem.split(' ')
-        first_numbers.append(splitted_numbers[0])
-        operators.append(splitted_numbers[1])
-        second_numbers.append(splitted_numbers[2])
-        
-    elif problem.find('-') is not None:
-        splitted_numbers = problem.split(' ')
-        first_numbers.append(splitted_numbers[0])
-        operators.append(splitted_numbers[1])
-        second_numbers.append(splitted_numbers[2])
-        
-for i in range(len(operators)):
-    if operators[i] == '+':
-        solutions.append(int(first_numbers[i]) + int(second_numbers[i]))
-    elif operators[i] == '-':
-        solutions.append(int(first_numbers[i]) - int(second_numbers[i]))
+    # Determine the length for dashes and proper formatting
+    for i in range(len(operators)):
+        dash_gap = max(len(first_numbers[i]), len(second_numbers[i])) + 2  # +2 for operator and space
+        dash_gaps.append(dash_gap)
 
-for i in range(len(operators)):
-    dash_gap = len(str(max(first_numbers[i], second_numbers[i]))) + 1 # -----
-    dash_gaps.append(dash_gap)
-    white_space = dash_gap - len(str(first_numbers[i])) # white-space
-    final_string += " " * white_space + first_numbers[i] + " " * (vertical_gap + 1)
-final_string += "\n"    
+    # Prepare the first line (first numbers)
+    for i in range(len(operators)):
+        difference = dash_gaps[i] - len(first_numbers[i])
+        final_string += " " * difference + first_numbers[i] + " " * vertical_gap
+    final_string += "\n"
 
-for i in range (len(operators)):
-    white_space = dash_gaps[i] - len(str(second_numbers[i]))
-    if operators[i] == '+':
-        final_string += "+" + " " * white_space + second_numbers[i] + " " * vertical_gap
-    elif operators[i] == '-':
-        final_string += "-" + " " * white_space + second_numbers[i] + " " * vertical_gap
-final_string += "\n"
+    # Prepare the second line (operators and second numbers)
+    for i in range(len(operators)):
+        blank_space = dash_gaps[i] - len(second_numbers[i]) - 1  # -1 for the operator
+        final_string += operators[i] + " " * blank_space + second_numbers[i] + " " * vertical_gap
+    final_string += "\n"
 
-for i in range(len(operators)):
-    final_string += "-" * (dash_gaps[i] + 1) + " " * vertical_gap
-final_string += "\n"
+    # Prepare the third line (dashes)
+    for i in range(len(operators)):
+        final_string += "-" * dash_gaps[i] + " " * vertical_gap
+    final_string += "\n"
 
-for i in range(len(operators)):
-    solution_space = dash_gaps[i] - len(str(solutions[i])) + 1  # Align solutions under the dashes
-    final_string += " " * solution_space + str(solutions[i]) + " " * vertical_gap
-final_string += "\n"
+    # Optionally, prepare the fourth line (solutions)
+    if show_answers:
+        for i in range(len(operators)):
+            solution_space = dash_gaps[i] - len(str(solutions[i]))
+            final_string += " " * solution_space + str(solutions[i]) + " " * vertical_gap
 
-print("First Numbers: ", first_numbers)
-print("Second Numbers: ", second_numbers)
-print("Operators: ", operators)
-print("Solutions: ", solutions)
-print("Dash Gaps: ", dash_gaps)
-print("Final String:\n", final_string)
+    return final_string
+
+# Test the function
+print(f'\n{arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"], show_answers=True)}')
+
+print(f'\n{arithmetic_arranger(["32 + 8", "1 - 3801", "9999 + 9999", "523 - 49"], True)}')
